330f77b9b07ee700e5f90b55702500b5d2357d76
PenielY <96361630+Pequanta@users.noreply.github.com>
2025-01-30 15:01:04 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 30-Jan-2025/Longest Common Prefix 226431.py
@@ -0,0 +1,11 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = strs[0]
+        for i in range(len(strs)):
+            j = 0
+            while j < len(res) and j < len(strs[i]) and res[j] == strs[i][j]:
+                j += 1
+            res = res[:j]
+        return res
