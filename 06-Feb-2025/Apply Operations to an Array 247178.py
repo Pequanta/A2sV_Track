ef7e24b6c0d33a5a352ba61b7acf2c9c5c363326
PenielY <96361630+Pequanta@users.noreply.github.com>
2025-02-06 17:14:00 +0300

    Created 'Apply Operations to an Array' by A2SV Hub

---
+++ 06-Feb-2025/Apply Operations to an Array 247178.py
@@ -0,0 +1,18 @@
+# Problem: Apply Operations to an Array - https://leetcode.com/problems/apply-operations-to-an-array/
+
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        count_zeros = 0
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+            if nums[i] == 0:
+                count_zeros += 1
+        result = [0] * len(nums)
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                result[index] = nums[i]
+                index += 1
+        return result
