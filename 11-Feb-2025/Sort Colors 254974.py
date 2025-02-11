7a6ea7b2f872738b2950a834d52f1000d06c8b93
PenielY <96361630+Pequanta@users.noreply.github.com>
2025-02-11 09:30:38 +0300

    Created 'Sort Colors' by A2SV Hub

---
+++ 11-Feb-2025/Sort Colors 254974.py
@@ -0,0 +1,12 @@
+# Problem: Sort Colors - https://leetcode.com/problems/sort-colors/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] >  nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
+        return nums
